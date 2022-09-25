4a2d37f1199e5b57a25694434f7480077f83fac7
Anish Programmer <anishtdubey@gmail.com>
2022-09-26 00:26:21 +0530

    school ke liye

---
+++ SchoolWork/ElectricityBillModified.java
@@ -0,0 +1,39 @@
+package SchoolWork;
+
+import java.util.Scanner;
+
+public class ElectricityBillModified {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        short standardCharge = 250;
+
+        System.out.print("Please enter your name: ");
+        String name = inp.next();
+
+        System.out.print("Enter units consumed: ");
+        double units = inp.nextDouble();
+
+        System.out.println("\n" + "PRESS 1 to calculate bill: ");
+        System.out.print("YOUR RESPONSE: ");
+        short choice = inp.nextShort();
+
+        switch (choice) {
+            case 1:
+                System.out.println("\n" + "The name of cutomer: " + name);
+                System.out.println("Number of units consumed: " + units);
+
+                double bill;
+                if (units < 100) {
+                    bill = 1 * units + standardCharge;
+                    System.out.println("The total bill is of: " + bill);
+                } else if (units >= 100 && units <= 200) {
+                    bill = 1 * 99 + (units - 99) * 2 + standardCharge;
+                    System.out.println("The total bill is of: " + bill);
+                } else if (units > 200) {
+                    bill = 1 * 99 + 2 * (200 - 99) + (units - 200) * 3 + standardCharge;
+                    System.out.println("The total bill is of: " + bill);
+                }
+        }
+    }
+}
